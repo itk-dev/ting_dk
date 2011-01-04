5197f9e87cc9763a74ac9f00da51d7ec622e79dc
Brian Jensen <mail@magnify.dk>
2011-01-04 10:36:05 +0100

    Inherit theme settings and styling of font on search button

---
+++ sites/all/themes/ting/theme-settings.php
@@ -0,0 +1,12 @@
+<?php
+// $Id$
+/**
+ * @file
+ * include the theme settings from mothership
+ */
+/*
+This is a shameless copy from the zen subtheme (starterkit) theme-settings
+go grap the code and send john Albin a beer :)
+drupal.org/project/zen
+ */
+include_once './' . drupal_get_path('theme', 'fusion_core') . '/theme-settings.php';
