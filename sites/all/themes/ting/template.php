ae0a2ad10599f537b473cf251cc7ccf49b2f9227
Martin Yde Granath <myg@aarhus.dk>
2011-02-10 11:46:03 +0100

    added system_perm module and moved ie style fixes

--- sites/all/themes/ting/template.php
+++ sites/all/themes/ting/template.php
@@ -14,4 +14,38 @@ function ting_breadcrumb($breadcrumb) {
   }
 }
 
+function ting_preprocess(&$vars, $hook) {
+
+    global $theme_info;
+    // Get the path to the theme to make the code more efficient and simple.
+    $path = drupal_get_path('theme', $theme_info->name);
+    
+    // conditional styles
+    // xpressions documentation  -> http://msdn.microsoft.com/en-us/library/ms537512.aspx
+
+    // syntax for .info
+    // top stylesheets[all][] = style/reset.css
+    // ie stylesheets[ condition ][all][] = ie6.css
+    // ------------------------------------------------------------------------
+
+    // Check for IE conditional stylesheets.
+
+    if (isset($theme_info->info['ie stylesheets'])) {
+      $ie_css = array();
+
+      // Format the array to be compatible with drupal_get_css().
+      foreach ($theme_info->info['ie stylesheets'] as $condition => $media) {
+        foreach ($media as $type => $styles) {
+          foreach ($styles as $style) {
+            $ie_css[$condition][$type]['theme'][$path . '/' . $style] = TRUE;
+          }
+        }
+      }
+      // Append the stylesheets to $styles, grouping by IE version and applying
+      // the proper wrapper.
+      foreach ($ie_css as $condition => $styles) {
+        $vars['styles'] .= '<!--[' . $condition . ']>' . "\n" . drupal_get_css($styles) . '<![endif]-->' . "\n";
+      }
+    }
+}
 ?>
