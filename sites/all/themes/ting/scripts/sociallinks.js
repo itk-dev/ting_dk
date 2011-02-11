a961a375908af0bb18d3f1b7da1fe639d8f5a983
Martin Yde Granath <myg@aarhus.dk>
2011-02-11 14:45:30 +0100

    added new social links

---
+++ sites/all/themes/ting/scripts/sociallinks.js
@@ -0,0 +1,47 @@
+/* 
+ * Social links script for front page.
+ */
+
+<!--
+$(document).ready(function() {
+// list animation
+var fadeDuration =200; //time in milliseconds
+$('.delicious').hover(function() {
+$(this).animate({ paddingTop: '0px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -0 }, fadeDuration);
+}, function() {
+$(this).animate({ paddingTop: '24px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -24 }, fadeDuration).fadeOut(fadeDuration);
+});
+$('.rss').hover(function() {
+$(this).animate({ paddingTop: '0px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -0 }, fadeDuration);
+}, function() {
+$(this).animate({ paddingTop: '24px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -24 }, fadeDuration).fadeOut(fadeDuration);
+});
+$('.twitter').hover(function() {
+$(this).animate({ paddingTop: '0px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -0 }, fadeDuration);
+}, function() {
+$(this).animate({ paddingTop: '24px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -24 }, fadeDuration).fadeOut(fadeDuration);
+});
+$('.facebook').hover(function() {
+$(this).animate({ paddingTop: '0px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -0 }, fadeDuration);
+}, function() {
+$(this).animate({ paddingTop: '24px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -24 }, fadeDuration).fadeOut(fadeDuration);
+});
+$('.stumbleupon').hover(function() {
+$(this).animate({ paddingTop: '0px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -0 }, fadeDuration);
+}, function() {
+$(this).animate({ paddingTop: '24px' }, fadeDuration);
+//$(this).children('span').show().animate({ top: -24 }, fadeDuration).fadeOut(fadeDuration);
+});
+
+});//doc ready
+// -->
+
