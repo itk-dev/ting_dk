1d962e3315243c03e17886c5a419eea5148fca4a
Mikkel Ricky <rimi@aarhus.dk>
2016-11-04 13:25:34 +0100

    SERV-50: Fixed router to look like the one used in Drupal 7

--- router.php
+++ router.php
@@ -1,19 +1,31 @@
 <?php
-// $Id: index.php,v 1.94 2007/12/26 08:46:48 dries Exp $
 
 /**
  * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * The routines here dispatch control to the appropriate handler, which then
- * prints the appropriate page.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt.
+ * Router (rewrite) for Drupal 6 with clean urls.
  */
 
-if (!empty($_SERVER['REQUEST_URI'])) {
-  list($path) = explode('?', $_SERVER['REQUEST_URI'], 2);
+/*
+ * Handle the case when urls are appended to the script (i.e. /router.php/«path»
+ * rather than /router.php?q=«path»).
+ * Code taken from Drupal 7:
+ * https://github.com/drupal/drupal/blob/7.x/includes/bootstrap.inc#L3091-L3105
+ */
+if (isset($_SERVER['REQUEST_URI'])) {
+  $request_path = strtok($_SERVER['REQUEST_URI'], '?');
+  $base_path = rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/');
+  $base_path_len = strlen($base_path);
+  // $base_path_len = strlen(rtrim(dirname($_SERVER['SCRIPT_NAME']), '\/'));
+  // Unescape and strip $base_path prefix, leaving q without a leading slash.
+  $path = substr(urldecode($request_path), $base_path_len + 1);
+  // If the path equals the script filename, either because 'index.php' was
+  // explicitly provided in the URL, or because the server added it to
+  // $_SERVER['REQUEST_URI'] even when it wasn't provided in the URL (some
+  // versions of Microsoft IIS do this), the front page should be served.
+  if ($path == basename($_SERVER['PHP_SELF'])) {
+    $path = '';
+  }
+
   $_REQUEST['q'] = $_GET['q'] = trim($path, '/');
 }
 
